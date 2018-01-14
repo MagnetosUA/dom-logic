dad147400217bb971ddf2c282b68f8487de2bc6c
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-01-14 22:16:30 +0200

    Adapting menu 1

--- src/MagnetosCompany/DispatcherBundle/Controller/DevicesController.php
+++ src/MagnetosCompany/DispatcherBundle/Controller/DevicesController.php
@@ -6,8 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DevicesController extends Controller
 {
-    public function dispatcherAction()
+    public function devicesAction()
     {
-        return $this->render('@Dispatcher/main.html.twig');
+        return $this->render('@Dispatcher/Default/device_manager.html.twig', [
+            'thing' => 'devices',
+        ]);
+    }
+
+    public function interfacesAction()
+    {
+        return $this->render('@Dispatcher/Default/device_manager.html.twig', [
+            'thing' => 'interfaces',
+        ]);
     }
 }

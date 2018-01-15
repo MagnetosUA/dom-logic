ab9282b91e852cfb1b8da2a4f9ca19f54cf83d07
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-01-15 23:50:31 +0200

    save last changes

--- src/MagnetosCompany/DispatcherBundle/Controller/DevicesController.php
+++ src/MagnetosCompany/DispatcherBundle/Controller/DevicesController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DevicesController extends Controller
 {
+    #devices
+
     public function devicesAction()
     {
         return $this->render('@Dispatcher/Default/device_manager.html.twig', [
@@ -13,10 +15,44 @@ class DevicesController extends Controller
         ]);
     }
 
+    public function sensorsAction()
+    {
+        return $this->render('@Dispatcher/Default/device_manager.html.twig', [
+            'thing' => 'devices',
+            'sensors' => true,
+        ]);
+    }
+
+    public function activatorsAction()
+    {
+        return $this->render('@Dispatcher/Default/device_manager.html.twig', [
+            'thing' => 'devices',
+            'activators' => true,
+        ]);
+    }
+
+    #interfaces
+
     public function interfacesAction()
     {
         return $this->render('@Dispatcher/Default/device_manager.html.twig', [
             'thing' => 'interfaces',
+        ]);
+    }
+
+    public function onewireAction()
+    {
+        return $this->render('@Dispatcher/Default/device_manager.html.twig', [
+            'thing' => 'interfaces',
+            'onewire' => true,
+        ]);
+    }
+
+    public function wifiAction()
+    {
+        return $this->render('@Dispatcher/Default/device_manager.html.twig', [
+            'thing' => 'interfaces',
+            'wifi' => true,
         ]);
     }
 }
